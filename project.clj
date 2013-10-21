2cd88154ff6ccc8a94b9860487adeb27230ebdf2
Patrick Carlisle <patrick@puppetlabs.com>
2013-10-21 13:32:59 -0700

    Set the version using git

--- project.clj
+++ project.clj
@@ -1,10 +1,24 @@
-(defproject classifier "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+(require '[clojure.string :as string])
+(require '[clojure.java.io :refer [file]])
+(require '[clojure.java.shell :refer [sh]])
+
+(def version-string
+  (memoize
+  (fn []
+    "Determine the version number using 'rake version -s'"
+    (if (.exists (file "version"))
+      (string/trim (slurp "version"))
+      (let [command ["rake" "package:version" "-s"]
+            {:keys [exit out err]} (apply sh command)]
+        (if (zero? exit)
+          (string/trim out)
+          "0.0-dev-build"))))))
+
+(defproject classifier (version-string)
+  :description "Node classifier"
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [ring/ring-jetty-adapter "1.2.0"]
                  [compojure "1.1.5"]]
   :profiles {:dev {:dependencies [[ring-mock "0.1.5"]]}}
+  :aot [puppetlabs.classifier.core]
   :main puppetlabs.classifier.core)
