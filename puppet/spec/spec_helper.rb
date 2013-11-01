23d3b2d7a7ccbcdaf7c2be740011322e0dbfa4bd
Patrick Carlisle <patrick@puppetlabs.com>
2013-11-01 10:39:28 -0700

    Remove dependency on puppetlabs_spec_helper

--- puppet/spec/spec_helper.rb
+++ puppet/spec/spec_helper.rb
@@ -1,1 +1,24 @@
-require 'puppetlabs_spec_helper/puppet_spec_helper'
+require 'puppet'
+require 'puppet/test/test_helper'
+
+RSpec.configure do |config|
+  config.mock_with(:mocha)
+
+  Puppet::Test::TestHelper.initialize
+
+  config.before(:all) do
+    Puppet::Test::TestHelper.before_all_tests
+  end
+
+  config.after(:all) do
+    Puppet::Test::TestHelper.after_all_tests
+  end
+
+  config.before(:each) do
+    Puppet::Test::TestHelper.before_each_test
+  end
+
+  config.after(:each) do
+    Puppet::Test::TestHelper.after_each_test
+  end
+end
