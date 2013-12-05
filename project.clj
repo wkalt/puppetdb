7f4865e8f7fa07c739d36626c8bbc41af44b34e5
Patrick Carlisle <patrick@puppetlabs.com>
2013-12-05 13:58:30 -0800

    Fix project dependencies
    The nexus repositories were missing and there were some minor version
    conflicts.

--- project.clj
+++ project.clj
@@ -18,7 +18,7 @@
   :description "Node classifier"
   :pedantic? :abort
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [compojure "1.1.5"]
+                 [compojure "1.1.6" :exclusions [[clj-time]]]
                  ;; Logging
                  [org.clojure/tools.logging "0.2.6"]
                  [org.clojure/java.jdbc "0.3.0-beta1"]
@@ -27,8 +27,10 @@
                  [cheshire "5.2.0"]
                  [puppetlabs/trapperkeeper "0.1.0-SNAPSHOT"]]
   :profiles {:dev {:dependencies [[ring-mock "0.1.5"]
-                                  [spyscope "0.1.3"]]
+                                  [spyscope "0.1.3" :exclusions [[clj-time]]]]
                    :injections [(require 'spyscope.core)]}}
+  :repositories [["releases" "http://nexus.delivery.puppetlabs.net/content/repositories/releases/"]
+                 ["snapshots" "http://nexus.delivery.puppetlabs.net/content/repositories/snapshots/"]]
   :test-selectors {:default (complement :database)
                    :database :database
                    :all (constantly true)}
