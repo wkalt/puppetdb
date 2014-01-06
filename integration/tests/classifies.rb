6ea6d8ef84fe674c2edf61c15417d5b2b07a3e4c
Patrick Carlisle <patrick@puppetlabs.com>
2014-01-06 11:34:35 -0800

    Set up classification test to work with multiple agents

--- integration/tests/classifies.rb
+++ integration/tests/classifies.rb
@@ -32,23 +32,23 @@ end
 
 Classifier.base_uri "#{database.reachable_name}:#{CLASSIFIER_PORT}"
 
+class_response = Classifier.put(
+  "/v1/classes/foo",
+  :body => {"parameters" => {}}.to_json)
+assert(class_response.response.is_a?(Net::HTTPSuccess),
+       "Received failure response when trying to create the class: " +
+       "HTTP Code #{class_response.code}: #{class_response.message}")
+
+group_response = Classifier.put(
+  "/v1/groups/foogroup",
+  :body => {"classes" => ["foo"]}.to_json)
+assert(group_response.response.is_a?(Net::HTTPSuccess),
+       "Received failure response when trying to create the group: " +
+       "HTTP Code #{group_response.code}: #{group_response.message}")
+
 with_puppet_running_on(master, master_opts, testdir) do
   agents.each do |agent|
     node_name = on(agent, 'puppet agent --configprint node_name_value').stdout.strip
-
-    class_response = Classifier.put(
-      "/v1/classes/foo",
-      :body => {"parameters" => {}}.to_json)
-    assert(class_response.response.is_a?(Net::HTTPSuccess),
-           "Received failure response when trying to create the class: " +
-           "HTTP Code #{class_response.code}: #{class_response.message}")
-
-    group_response = Classifier.put(
-      "/v1/groups/foogroup",
-      :body => {"classes" => ["foo"]}.to_json)
-    assert(group_response.response.is_a?(Net::HTTPSuccess),
-           "Received failure response when trying to create the group: " +
-           "HTTP Code #{group_response.code}: #{group_response.message}")
 
     rule_response = Classifier.post(
       "/v1/rules",
