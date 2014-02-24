585d7325a61cbfb94ee7e00e205bb77149555620
Dan Lidral-Porter <dan.lidral-porter@puppetlabs.com>
2014-02-24 11:08:49 -0800

    (maint) sql aggregation utils preserve row order.
    Due to the use of group-by, the aggregate-* sql utils did not preserve
    the order of the rows passed in if there were more than eight groups,
    since this is the point at which clojure switches from using an ArrayMap
    (which preserves insertion order) to a HashMap (which does not). Since
    none of our tests exceeded this threshold, it was not noticed before.
    
    Add ordered-group-by to the sql-utils ns, which is similar to group-by,
    but adds the first index that a member of the group was seen at to the
    beginning of the vector with the group members, and returns a seq of map
    entries sorted by this index (rather than a map).
    
    Augment the aggregate-column test so it has enough data to overflow the
    ArrayMap size and use a HashMap, which will mess up ordering if we're
    not careful.

--- src/puppetlabs/classifier/storage/sql_utils.clj
+++ src/puppetlabs/classifier/storage/sql_utils.clj
@@ -1,13 +1,27 @@
 (ns puppetlabs.classifier.storage.sql-utils)
 
+(defn ordered-group-by
+  [f coll]
+  (let [grouped-w-index (loop [i 0, groups (transient {}), coll (seq coll)]
+                          (if-let [x (first coll)]
+                            (let [k (f x)
+                                  group (get groups k [i])
+                                  groups' (assoc! groups k (conj group x))]
+                              (recur (inc i) groups' (next coll)))
+                            ;; else (nothing left in coll)
+                            (persistent! groups)))]
+    (->> (seq grouped-w-index)
+      ; sort the groups by the index where the first member appeared
+      (sort-by #(get-in % [1 0])))))
+
 (defn aggregate-submap-by
   "Given a sequence of maps in results where each map contains agg-key and
   agg-val as keys, groups the maps that are identical except for the values in
   agg-key or agg-val. The values of agg-key and agg-val are turned into a map
   and stored in the resulting map under under-key."
   [agg-key agg-val under-key results]
-  (for [[grouped all] (group-by #(dissoc % agg-key agg-val) results)]
-    (assoc grouped under-key (->> all
+  (for [[combined [_ & all]] (ordered-group-by #(dissoc % agg-key agg-val) results)]
+    (assoc combined under-key (->> all
                                (map (juxt agg-key agg-val))
                                (remove (comp nil? first))
                                (into {})))))
@@ -17,5 +31,5 @@
   a sequence under `under`, combining rows that are equal except for the value
   of `column`. Useful for consolidating the results of an outer join."
   [column under results]
-  (for [[combined column-differs] (group-by #(dissoc % column) results)]
-    (assoc combined under (map #(get % column) column-differs))))
+  (for [[combined [_ & all]] (ordered-group-by #(dissoc % column) results)]
+    (assoc combined under (map #(get % column) all))))
--- test/puppetlabs/classifier/storage/sql_utils_test.clj
+++ test/puppetlabs/classifier/storage/sql_utils_test.clj
@@ -35,9 +35,32 @@
                (keywordize-keys)))))))
 
 (deftest column-aggregation
-  (testing "aggregates as expected"
+  (testing "aggregates as expected while preserving order"
     (let [rows [{:white 1 :blue 2 :green 0}
+                {:white 1 :blue 4 :green 1}
+                {:white 1 :blue 6 :green 2}
+                {:white 1 :blue 8 :green 3}
+                {:white 1 :blue 10 :green 4}
+                {:white 1 :blue 12 :green 5}
+                {:white 1 :blue 14 :green 6}
+                {:white 1 :blue 16 :green 7}
+                {:white 1 :blue 18 :green 8}
+                {:white 1 :blue 7 :green 2}
+                {:white 1 :blue 19 :green 8}
+                {:white 1 :blue 13 :green 5}
+                {:white 1 :blue 15 :green 6}
+                {:white 1 :blue 5 :green 1}
+                {:white 1 :blue 11 :green 4}
                 {:white 1 :blue 3 :green 0}
-                {:white 1 :blue 4 :green 1}]]
-      (is (= [{:white 1 :blues [2 3] :green 0} {:white 1 :blues [4] :green 1}]
+                {:white 1 :blue 17 :green 7}
+                {:white 1 :blue 9 :green 3}]]
+      (is (= [{:white 1 :blues [2 3] :green 0}
+              {:white 1 :blues [4 5] :green 1}
+              {:white 1 :blues [6 7] :green 2}
+              {:white 1 :blues [8 9] :green 3}
+              {:white 1 :blues [10 11] :green 4}
+              {:white 1 :blues [12 13] :green 5}
+              {:white 1 :blues [14 15] :green 6}
+              {:white 1 :blues [16 17] :green 7}
+              {:white 1 :blues [18 19] :green 8}]
              (aggregate-column :blue :blues rows))))))
