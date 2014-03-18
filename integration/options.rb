8611b7a7f0252ae8ae2c2fa764100498068f1b69
Patrick Carlisle <patrick@puppetlabs.com>
2014-03-17 17:51:10 -0700

    Add add_el_extras to default integration config

--- integration/options.rb
+++ integration/options.rb
@@ -4,6 +4,7 @@ integration_root = File.expand_path(File.dirname(options_file_path))
   :helper => [File.join(integration_root, 'helper.rb')],
   :pre_suite => [File.join(integration_root, 'setup', 'early'),
                  File.join(integration_root, 'setup', 'pre_suite')],
+  :add_el_extras => true,
   :classifier_repo_puppet => "https://github.com/puppetlabs/puppet.git",
   :classifier_repo_hiera => "https://github.com/puppetlabs/hiera.git",
   :classifier_repo_facter => "https://github.com/puppetlabs/facter.git",
