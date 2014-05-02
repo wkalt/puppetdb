573a9488a05aa5058f07a19c9538ba66476659d9
Jenkins User <jenkins@debian-7-x64.acctest.dc1.puppetlabs.net>
2014-05-02 13:44:53 -0700

    lein-release plugin: preparing 0.3.0 release

--- project.clj
+++ project.clj
@@ -11,7 +11,7 @@
    :password :env/nexus_jenkins_password
    :sign-releases false})
 
-(defproject puppetlabs/classifier "0.3.0-SNAPSHOT"
+(defproject puppetlabs/classifier "0.3.0"
   :description "Node classifier"
   :pedantic? :abort
   :dependencies [[org.clojure/clojure "1.5.1"]
