4dfaa65859d64da2515050d738c3e8b332afa34f
Tony Vu <tony.vu@puppetlabs.com>
2014-07-11 15:14:20 -0700

    Pin puppetlabs/postgresql version to 3.3.0
    The latest version, 3.4.0, has breaking changes in rhel based systems that
    breaks the pre-suites for integration.

--- integration/setup/pe_pre_suite/35_install_postgres_module.rb
+++ integration/setup/pe_pre_suite/35_install_postgres_module.rb
@@ -1,3 +1,3 @@
 step "Install the postgres module" do
-    on database, "puppet module upgrade puppetlabs/postgresql"
+    on database, "puppet module upgrade puppetlabs/postgresql --version 3.3.0 --force"
 end
--- integration/setup/pre_suite/50_install_modules.rb
+++ integration/setup/pre_suite/50_install_modules.rb
@@ -1,3 +1,3 @@
 step "Install the postgres module" do
-  on database, "puppet module install puppetlabs/postgresql"
+  on database, "puppet module install puppetlabs/postgresql --version 3.3.0"
 end
