09c55e6006da536f19cfc898cbe64df4efb3ff4c
Jenkins User <jenkins@debian-7-x64.acctest.dc1.puppetlabs.net>
2014-08-25 16:01:51 -0700

    lein-release plugin: bumped version from 0.6.2 to 0.6.3-SNAPSHOT for next development cycle

--- project.clj
+++ project.clj
@@ -12,7 +12,7 @@
    :password :env/nexus_jenkins_password
    :sign-releases false})
 
-(defproject puppetlabs/classifier "0.6.2"
+(defproject puppetlabs/classifier "0.6.3-SNAPSHOT"
   :description "Node classifier"
   :pedantic? :abort
   :dependencies [[org.clojure/clojure "1.6.0"]
