43bc24e89fd67c5b0dcdaff6137506fc7ed1b634
Jenkins CI <ci@puppetlabs.com>
2014-09-05 09:53:19 -0700

    lein-release plugin: preparing 0.6.5 release

--- project.clj
+++ project.clj
@@ -13,7 +13,7 @@
    :password :env/nexus_jenkins_password
    :sign-releases false})
 
-(defproject puppetlabs/classifier "0.6.5-SNAPSHOT"
+(defproject puppetlabs/classifier "0.6.5"
   :description "Node classifier"
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [org.clojure/tools.logging "0.2.6"]
