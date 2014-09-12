47ce43b60f037628dc27453e0a442457c1222756
Jenkins CI <ci@puppetlabs.com>
2014-09-12 14:49:56 -0700

    lein-release plugin: bumped version from 0.7.2 to 0.7.3-SNAPSHOT for next development cycle

--- project.clj
+++ project.clj
@@ -13,7 +13,7 @@
    :password :env/nexus_jenkins_password
    :sign-releases false})
 
-(defproject puppetlabs/classifier "0.7.2"
+(defproject puppetlabs/classifier "0.7.3-SNAPSHOT"
   :description "Node classifier"
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [org.clojure/tools.logging "0.2.6"]
