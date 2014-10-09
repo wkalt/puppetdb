6e776341eecf646f64ec070661b34ab952e3f077
wkalt <wyaltster@gmail.com>
2014-10-09 16:43:17 -0700

    adf

--- acceptance/setup/pre_suite/20_install_puppet.rb
+++ acceptance/setup/pre_suite/20_install_puppet.rb
@@ -13,8 +13,9 @@ test_name "Install Puppet" do
 
   with_puppet_running_on(
     master,
-    {:master => {:dns_alt_names => "puppet,#{master_facts['hostname']},#{master_facts['fqdn']}",
-                :trace => 'true'}}) do
+    :master => {:dns_alt_names => "puppet,#{master_facts['hostname']},#{master_facts['fqdn']}",
+                :trace => 'true'}
+    :commandline_args => '--debug') do
     # PID file exists?
     step "PID file created?" do
       on master, "[ -f #{pidfile} ]"
