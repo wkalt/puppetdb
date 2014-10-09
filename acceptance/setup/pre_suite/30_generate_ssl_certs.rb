027008ef2f72bb12ce24ef559a5b45b2ea7ea370
wkalt <wyaltster@gmail.com>
2014-10-09 11:41:00 -0700

    reformat args

--- acceptance/setup/pre_suite/30_generate_ssl_certs.rb
+++ acceptance/setup/pre_suite/30_generate_ssl_certs.rb
@@ -1,6 +1,7 @@
 step "Run an agent to create the SSL certs" do
-  with_puppet_running_on master, {'master' => {'autosign' => 'true' 'trace' => true}
-                                  :commandline_args => "--debug"} do
+  with_puppet_running_on master,
+    :master => {'autosign' => 'true' 'trace' => true}
+  :commandline_args => "--debug"} do
     run_agent_on database, "--test"
   end
 end
