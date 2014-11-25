590f9c20d64b880870ee710ba0a8a45d9a752880
wkalt <wyaltster@gmail.com>
2014-11-24 17:11:18 -0800

    some progress

--- src/puppetdb_sync/command.clj
+++ src/puppetdb_sync/command.clj
@@ -1,68 +1,15 @@
 (ns puppetlabs.puppetdb.command
-  "PuppetDB command handling
-
-   Commands are the mechanism by which changes are made to PuppetDB's
-   model of a population. Commands are represented by `command
-   objects`, which have the following JSON wire format:
-
-       {\"command\": \"...\",
-        \"version\": 123,
-        \"payload\": <json object>}
-
-   `payload` must be a valid JSON string of any sort. It's up to an
-   individual handler function how to interpret that object.
-
-   More details can be found in [the spec](../spec/commands.md).
-
-   The command object may also contain an `annotations` attribute
-   containing a map with arbitrary keys and values which may have
-   command-specific meaning or may be used by the message processing
-   framework itself.
-
-   Commands should include a `received` annotation containing a
-   timestamp of when the message was first seen by the system. If this
-   is omitted, it will be added when the message is first parsed, but
-   may then be somewhat inaccurate.
-
-   Commands should include an `id` annotation containing a unique,
-   string identifier for the command. If this is omitted, it will be
-   added when the message is first parsed.
-
-   Failed messages will have an `attempts` annotation containing an
-   array of maps of the form:
-
-       {:timestamp <timestamp>
-        :error     \"some error message\"
-        :trace     <stack trace from :exception>}
-
-   Each entry corresponds to a single failed attempt at handling the
-   message, containing the error message, stack trace, and timestamp
-   for each failure. PuppetDB may discard messages which have been
-   attempted and failed too many times, or which have experienced
-   fatal errors (including unparseable messages).
-
-   Failed messages will be stored in files in the \"dead letter
-   office\", located under the MQ data directory, in
-   `/discarded/<command>`. These files contain the annotated message,
-   along with each exception that occured while trying to handle the
-   message.
-
-   We currently support the following wire formats for commands:
-
-   1. Java Strings
-
-   2. UTF-8 encoded byte-array
-
-   In either case, the command itself, once string-ified, must be a
-   JSON-formatted string with the aforementioned structure."
   (:require [clojure.tools.logging :as log]
             [puppetlabs.puppetdb.scf.storage :as scf-storage]
             [puppetlabs.puppetdb.catalogs :as cat]
             [puppetlabs.puppetdb.reports :as report]
             [puppetlabs.puppetdb.facts :as facts]
+            [puppetlabs.puppetdb.cli.export :as export]
             [puppetlabs.puppetdb.command.dlo :as dlo]
             [puppetlabs.puppetdb.mq :as mq]
             [puppetlabs.kitchensink.core :as kitchensink]
+            [clojure.set :as set]
+            [clj-http.util :refer [url-encode]]
             [clj-http.client :as client]
             [clj-time.coerce :refer [to-timestamp]]
             [puppetlabs.puppetdb.config :as conf]
@@ -80,26 +27,82 @@
 
 (def doc
   {:timestamp 0
+   :sender "localhost"
    :catalog-hashes (take 20 (repeatedly random-string))
    :report-hashes (take 20 (repeatedly random-string))
    :factset-hashes (take 20 (repeatedly random-string))})
 
-(def catalog-endpoint "/v4/catalogs")
-(def report-endpoint "/v4/reports")
-(def factsets-endpoint "/v4/factsetss")
+(def catalogs-endpoint "/v4/catalogs")
+(def reports-endpoint "/v4/reports")
+(def factsets-endpoint "/v4/factsets")
 (def sync-endpoint "/ha/v4/sync")
-(def remotehost "mbp.local")
 
-(defn get-hashes
-  []
+(defn transform-factset
+  [{:keys [certname environment producer-timestamp facts]}]
+  {:command "replace facts"
+   :version 3
+   :payload {:name certname
+             :environment environment
+             :producer-timestamp producer-timestamp
+             :values facts}})
 
-  
-  )
+(defn transform-catalog
+  [response]
+  {"command" "replace catalog"
+   "version" 5
+   "payload" (dissoc response "hash")})
+
+(defn transform-report
+  [response]
+  {"command" "submit report"
+   "version" 4
+   "payload" (dissoc response "hash" "receive-time" "start-time" "end-time")})
+
+(defn query-db 
+  [host endpoint query]
+  (let [{:keys [status body]}  (client/get (format "http://%s:8080%s?query=%s" host endpoint (url-encode (json/generate-string query))))]
+    (if (= status 200) (json/parse-string body) [])))
+
+(defn transfer-response
+  [local port payload]
+  (let [checksum (kitchensink/utf8-string->sha1 payload)
+        url (format "http://%s:%s/v4/commands?checksum=%s" local port checksum)]
+    (client/post url {:body               payload
+                      :throw-exceptions   false
+                      :content-type       :json
+                      :character-encoding "utf-8"
+                      :accept             :json})))
+
+(defn query-hash-and-transfer!
+  [remote local entity hash]
+  (let [[transform endpoint] (case entity
+                               :factsets [transform-factset factsets-endpoint]
+                               :reports [transform-report reports-endpoint]
+                               :catalogs [transform-catalog catalogs-endpoint]) 
+        response (first (query-db remote endpoint ["=" "hash" hash]))]
+    (transfer-response local 8080 (json/generate-string (transform response)))))
+
+
+(defn fetch-hashes
+  [host entity]
+  (let [operator (if (= entity :catalogs) "name" "certname")
+        endpoint (case entity
+                        :factsets factsets-endpoint
+                        :reports reports-endpoint
+                        :catalogs catalogs-endpoint)]
+    (map #(get % "hash") (query-db host endpoint ["extract" ["hash"] ["~" operator ".*"]]))))
 
 (defn puppetdb-sync*
-  [{:keys [timestamp catalog-hashes report-hashes factset-hashes]}]
-  (doseq [h catalog-hashes]
-    (let [{:keys [body]} (query-db a catalogs-endpoint ["=" "hash" h])
-          query-result (json/parse-stream body)]
-      ))
-  )
+  [{:keys [timestamp sender catalog-hashes report-hashes factset-hashes]}]
+  (let [catalog-targets (set/difference catalog-hashes (fetch-hashes "localhost" catalogs-endpoint ["extract" ["hash" ["~" "certname" ".*"]]]))
+        factset-targets (set/difference factset-hashes (fetch-hashes "localhost" factsets-endpoint ["extract" ["hash" ["~" "certname" ".*"]]]))
+        report-targets (set/difference report-hashes (fetch-hashes "localhost" reports-endpoint ["extract" ["hash" ["~" "certname" ".*"]]]))]
+
+    (doseq [h catalog-targets]
+      (query-hash-and-transfer! sender "localhost" :catalogs h))
+
+    (doseq [h report-targets]
+      (query-hash-and-transfer! sender "localhost" :reports h))
+
+    (doseq [h factset-targets]
+      (query-hash-and-transfer! sender "localhost" :factsets h))))
