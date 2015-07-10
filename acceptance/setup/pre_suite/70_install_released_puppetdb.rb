53927b5437ffd2a93b8e8e4dc98c3784fcd5e8b2
Wyatt Alt <wyaltster@gmail.com>
2015-07-10 09:46:37 -0700

    (maint) pin pre-upgrade pdb to 2.3.5
    This should prevent the 2.3.x upgrade test from trying to install 3.0.0.

--- acceptance/setup/pre_suite/70_install_released_puppetdb.rb
+++ acceptance/setup/pre_suite/70_install_released_puppetdb.rb
@@ -1,8 +1,8 @@
 # We skip this step entirely unless we are running in :upgrade mode.
 if (test_config[:install_mode] == :upgrade)
   step "Install most recent released PuppetDB on the PuppetDB server for upgrade test" do
-    install_puppetdb(database, test_config[:database], 'latest')
+    install_puppetdb(database, test_config[:database], '2.3.5')
     start_puppetdb(database)
-    install_puppetdb_termini(master, database, 'latest')
+    install_puppetdb_termini(master, database, '2.3.5')
   end
 end
