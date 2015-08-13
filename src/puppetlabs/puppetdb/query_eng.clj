bbce8d527f84eff513eeb5333da4c3f5698a2a45
wkalt <wyaltster@gmail.com>
2015-08-13 08:58:51 -0700

    broken

--- src/puppetlabs/puppetdb/query_eng.clj
+++ src/puppetlabs/puppetdb/query_eng.clj
@@ -107,14 +107,15 @@
 
 (defn stream-query-result
   "Given a query, and database connection, return a Ring response with the query
-  results."
+   results."
   ([entity version query paging-options db url-prefix]
    ;; We default to doall because tests need this for the most part
    (stream-query-result entity version query paging-options db url-prefix doall))
   ([entity version query paging-options db url-prefix row-fn]
-   (let [[query->sql munge-fn] (entity->sql-fns entity version paging-options url-prefix)]
+   (let [munge-fn (get-in entity-fn-idx [entity :munge])]
      (jdbc/with-transacted-connection db
-       (let [{:keys [results-query]} (query->sql query)]
+       (let [{:keys [results-query]}
+             (query->sql entity version query paging-options)]
          (jdbc/with-query-results-cursor results-query (comp row-fn munge-fn)))))))
 
 (defn produce-streaming-body
@@ -126,27 +127,32 @@
   GET request) or an already parsed clojure data structure (if it's
   from a POST request).
 
-  If the query can't be parsed, a 400 is returned."
+   If the query can't be parsed, a 400 is returned."
   {:deprecated "3.0.0"}
   [entity version query paging-options db url-prefix]
   (try
     (jdbc/with-transacted-connection db
-      (let [[query->sql munge-fn] (entity->sql-fns entity version paging-options url-prefix)
-            {:keys [results-query count-query]} (-> query json/coerce-from-json query->sql)
-            query-error (promise)
-            resp (http/streamed-response
-                  buffer
-                  (try (jdbc/with-transacted-connection db
-                         (jdbc/with-query-results-cursor
-                          results-query (comp #(http/stream-json % buffer)
-                                              #(do (first %) (deliver query-error nil) %)
-                                              munge-fn)))
-                       (catch java.sql.SQLException e
-                         (deliver query-error e))))]
-        (if @query-error
-          (throw @query-error)
-          (cond-> (http/json-response* resp)
-            count-query (http/add-headers {:count (jdbc/get-result-count count-query)})))))
+      (let [query-rec (get-in entity-fn-idx [entity :rec])
+            munge-fn (get-in entitity-fn-idx [entity :munge])]
+        (let [[query->sql munge-fn] (entity->sql-fns entity version
+                                                     paging-options url-prefix)
+              {:keys [results-query count-query]} (-> query
+                                                      json/coerce-from-json
+                                                      query->sql)
+              query-error (promise)
+              resp (http/streamed-response
+                     buffer
+                     (try (jdbc/with-transacted-connection db
+                            (jdbc/with-query-results-cursor
+                              results-query (comp #(http/stream-json % buffer)
+                                                  #(do (first %) (deliver query-error nil) %)
+                                                  munge-fn)))
+                          (catch java.sql.SQLException e
+                            (deliver query-error e))))]
+          (if @query-error
+            (throw @query-error)
+            (cond-> (http/json-response* resp)
+              count-query (http/add-headers {:count (jdbc/get-result-count count-query)}))))))
     (catch com.fasterxml.jackson.core.JsonParseException e
       (log/errorf e (str "Error executing query '%s' for entity '%s' "
                          "with paging-options '%s'. Returning a 400 error code.")
