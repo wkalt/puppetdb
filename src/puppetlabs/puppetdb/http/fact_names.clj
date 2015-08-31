d340f568df41f1ee9307f2411050ec2a8ddfb5e7
Wyatt Alt <wyaltster@gmail.com>
2015-08-31 10:33:38 -0700

    need to fix counts filter

--- src/puppetlabs/puppetdb/http/fact_names.clj
+++ src/puppetlabs/puppetdb/http/fact_names.clj
@@ -2,15 +2,39 @@
   (:require [puppetlabs.puppetdb.query.paging :as paging]
             [puppetlabs.puppetdb.http.query :as http-q]
             [net.cgrand.moustache :refer [app]]
+            [puppetlabs.puppetdb.query-eng :refer [produce-streaming-body']]
+            [clojure.walk :refer [keywordize-keys]]
+            [puppetlabs.puppetdb.utils :refer [assoc-when]]
             [puppetlabs.puppetdb.middleware :refer [verify-accepts-json
                                                     validate-query-params
                                                     wrap-with-paging-options]]))
+
+(defn query-route
+  "Returns a route for querying PuppetDB that supports the standard
+   paging parameters. Also accepts GETs and POSTs. Composes
+   `optional-handlers` with the middleware function that executes the
+   query."
+  [entity version & optional-handlers]
+  (app
+    http-q/extract-query
+    (apply comp
+           (fn [{:keys [params globals puppetdb-query]}]
+             (let [puppetdb-query (if (nil? (:order_by puppetdb-query))
+                                    (assoc puppetdb-query :order_by [[:name :ascending]])
+                                    puppetdb-query)]
+               (produce-streaming-body'
+                 entity
+                 version
+                 (http-q/validate-distinct-options! (merge (keywordize-keys params) puppetdb-query))
+                 (:scf-read-db globals)
+                 (:url-prefix globals))))
+           optional-handlers)))
 
 (defn routes
   [version]
   (app
     []
-    (http-q/query-route :fact-names version identity)))
+    (query-route :fact-names version identity)))
 
 (defn fact-names-app
   [version]
