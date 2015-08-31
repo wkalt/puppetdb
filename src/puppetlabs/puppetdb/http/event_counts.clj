f4c9c96666ea5e5b2048230124a322a75a465bd1
Wyatt Alt <wyaltster@gmail.com>
2015-08-30 20:48:48 -0700

    aggregate event counts working, not quite on postgres due to counts_filter issue

--- src/puppetlabs/puppetdb/http/event_counts.clj
+++ src/puppetlabs/puppetdb/http/event_counts.clj
@@ -1,35 +1,9 @@
 (ns puppetlabs.puppetdb.http.event-counts
-  (:require [puppetlabs.puppetdb.cheshire :as json]
-            [puppetlabs.puppetdb.http.events :as events-http]
-            [puppetlabs.puppetdb.query.paging :as paging]
-            [puppetlabs.puppetdb.query-eng :refer [produce-streaming-body]]
+  (:require [puppetlabs.puppetdb.query.paging :as paging]
             [puppetlabs.puppetdb.http.query :as http-q]
             [puppetlabs.puppetdb.middleware :refer [verify-accepts-json validate-query-params
                                                     wrap-with-paging-options]]
-            [clojure.tools.logging :as log]
             [net.cgrand.moustache :refer [app]]))
-
-(defn routes
-  [version]
-  (app
-   [""]
-   {:get (fn [{:keys [params globals paging-options]}]
-           (when (= "puppetdb" (:product-name globals))
-             (log/warn "The event-counts endpoint is experimental"
-                       " and may be altered or removed in the future."))
-           (let [{:strs [query summarize_by counts_filter count_by] :as query-params} params
-                 query-options (merge {:counts_filter (if counts_filter (json/parse-strict-string counts_filter true))
-                                       :count_by count_by
-                                       :summarize_by summarize_by}
-                                      (events-http/validate-distinct-options! query-params)
-                                      paging-options)]
-             (produce-streaming-body
-              :event-counts
-              version
-              query
-              query-options
-              (:scf-read-db globals)
-              (:url-prefix globals))))}))
 
 (defn routes
   [version optional-handlers]
