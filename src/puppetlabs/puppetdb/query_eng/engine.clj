660dea203c968f4ead2d89ed3afa14da6188f19c
wkalt <wyaltster@gmail.com>
2016-04-10 20:55:16 -0700

    add cached catalog status to nodes

--- src/puppetlabs/puppetdb/query_eng/engine.clj
+++ src/puppetlabs/puppetdb/query_eng/engine.clj
@@ -134,6 +134,9 @@
                              "latest_report_status" {:type :string
                                                      :queryable? true
                                                      :field :report_statuses.status}
+                             "latest_report_cached_catalog_status" {:type :string
+                                                                    :queryable? true
+                                                                    :field :reports.cached_catalog_status}
                              "catalog_environment" {:type :string
                                                     :queryable? true
                                                     :field :catalog_environment.environment}
